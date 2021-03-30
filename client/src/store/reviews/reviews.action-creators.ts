a52536bf949034aa2962a1aef295904292d5743d
for-alisia <for-alisia@yandex.ru>
2021-03-30 18:51:17 +0500

    Reviews: added action types for adding, deleting and editing

--- client/src/store/reviews/reviews.action-creators.ts
+++ client/src/store/reviews/reviews.action-creators.ts
@@ -3,6 +3,7 @@ import { ReviewAction } from './reviews.actions';
 import { ReviewsActionType } from './reviews.action-types';
 import { Review } from '../../models/Review.model';
 
+/** Fetching of reviews */
 export const fetchReviewsStart = (page: number, postsPerPage: number): ReviewAction => ({
   type: ReviewsActionType.FETCH_REVIEWS_START,
   payload: { page, postsPerPage },
@@ -21,4 +22,52 @@ export const fetchReviewsSuccess = (
 export const fetchReviewsFailed = (errorMsg: string): ReviewAction => ({
   type: ReviewsActionType.FETCH_REVIEWS_FAILED,
   payload: errorMsg,
+});
+
+/** Adding a new review */
+export const addReviewStart = (review: Review): ReviewAction => ({
+  type: ReviewsActionType.ADD_REVIEW_START,
+  payload: review,
+});
+
+export const addReviewFailed = (error: string): ReviewAction => ({
+  type: ReviewsActionType.ADD_REVIEW_FAILED,
+  payload: error,
+});
+
+export const addReviewSuccess = (review: Review): ReviewAction => ({
+  type: ReviewsActionType.ADD_REVIEW_SUCCESS,
+  payload: review,
+});
+
+/** Deleting a new review */
+export const deleteReviewStart = (reviewId: string): ReviewAction => ({
+  type: ReviewsActionType.DELETE_REVIEW_START,
+  payload: reviewId,
+});
+
+export const deleteReviewFailed = (error: string): ReviewAction => ({
+  type: ReviewsActionType.DELETE_REVIEW_FAILED,
+  payload: error,
+});
+
+export const deleteReviewSuccess = (message: string): ReviewAction => ({
+  type: ReviewsActionType.DELETE_REVIEW_SUCCESS,
+  payload: message,
+});
+
+/** Editing a new review */
+export const editReviewStart = (reviewId: string): ReviewAction => ({
+  type: ReviewsActionType.EDIT_REVIEW_START,
+  payload: reviewId,
+});
+
+export const editReviewFailed = (error: string): ReviewAction => ({
+  type: ReviewsActionType.EDIT_REVIEW_FAILED,
+  payload: error,
+});
+
+export const editReviewSuccess = (review: Review): ReviewAction => ({
+  type: ReviewsActionType.EDIT_REVIEW_SUCCESS,
+  payload: review,
 });
--- client/src/store/reviews/reviews.actions.ts
+++ client/src/store/reviews/reviews.actions.ts
@@ -4,6 +4,7 @@ import { ReviewsActionType } from './reviews.action-types';
 /** Models */
 import { Review } from '../../models/Review.model';
 
+/** Fetching of the existing Reviews */
 export interface FetchReviewsStart {
   type: ReviewsActionType.FETCH_REVIEWS_START;
   payload: { page: number; postsPerPage: number };
@@ -19,4 +20,66 @@ export interface FetchReviewsSuccess {
   payload: { reviews: Review[]; totalPages: number; currentPage: number; postsPerPage: number };
 }
 
-export type ReviewAction = FetchReviewsFailed | FetchReviewsStart | FetchReviewsSuccess;
+/** Creating a new Review */
+export interface AddReviewStart {
+  type: ReviewsActionType.ADD_REVIEW_START;
+  payload: Review;
+}
+
+export interface AddReviewFailed {
+  type: ReviewsActionType.ADD_REVIEW_FAILED;
+  payload: string;
+}
+
+export interface AddReviewSuccess {
+  type: ReviewsActionType.ADD_REVIEW_SUCCESS;
+  payload: Review;
+}
+
+/** Deleting a Review */
+
+export interface DeleteReviewStart {
+  type: ReviewsActionType.DELETE_REVIEW_START;
+  payload: string;
+}
+
+export interface DeleteReviewFailed {
+  type: ReviewsActionType.DELETE_REVIEW_FAILED;
+  payload: string;
+}
+
+export interface DeleteReviewSuccess {
+  type: ReviewsActionType.DELETE_REVIEW_SUCCESS;
+  payload: string;
+}
+
+/** Editing a Review */
+
+export interface EditReviewStart {
+  type: ReviewsActionType.EDIT_REVIEW_START;
+  payload: string;
+}
+
+export interface EditReviewFailed {
+  type: ReviewsActionType.EDIT_REVIEW_FAILED;
+  payload: string;
+}
+
+export interface EditReviewSuccess {
+  type: ReviewsActionType.EDIT_REVIEW_SUCCESS;
+  payload: Review;
+}
+
+export type ReviewAction =
+  | FetchReviewsFailed
+  | FetchReviewsStart
+  | FetchReviewsSuccess
+  | AddReviewStart
+  | AddReviewSuccess
+  | AddReviewFailed
+  | EditReviewFailed
+  | EditReviewSuccess
+  | EditReviewStart
+  | DeleteReviewFailed
+  | DeleteReviewStart
+  | DeleteReviewSuccess;
